e88bc81acdf97259f655caf7432eca59f0f71824
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2022-08-08 15:10:53 +0200

    Section(ionic component basics) - 12: finishing up the base javascript logic

--- 2- Ionic Component Basics/ionic-angular-course/app.js
+++ 2- Ionic Component Basics/ionic-angular-course/app.js
@@ -1,9 +1,11 @@
 const reasonInput = document.querySelector("#input-reason");
 const amountInput = document.querySelector("#input-amount");
 const expensesList = document.querySelector("#expenses-list");
+const totalExpensesOutput = document.querySelector("#total-expenses");
 const saveBtn = document.querySelector("#btn-save");
 const clearBtn = document.querySelector("#btn-clear");
 
+let totalExpenses = 0;
 
 saveBtn.addEventListener('click', () => {
     const enteredReasonInput = reasonInput.value;
@@ -16,8 +18,16 @@ saveBtn.addEventListener('click', () => {
     const newIonItem = document.createElement('ion-item');
     newIonItem.textContent = enteredReasonInput + ': $' + enteredAmountInput;
     expensesList.appendChild(newIonItem);
+
+    totalExpenses += +enteredAmountInput;
+    totalExpensesOutput.textContent = totalExpenses;
+
+    clearInput();
 });
 
-clearBtn.addEventListener('click', () => {
-    console.log('clear btn clicked');
-});
+const clearInput = () => {
+    reasonInput.value = '';
+    amountInput.value = '';
+}
+
+clearBtn.addEventListener('click', clearInput);

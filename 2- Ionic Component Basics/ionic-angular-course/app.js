c2ac60b3ece68c5c20eb0b0033b6616852113fc1
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2022-08-08 14:43:49 +0200

    Section(ionic component basics) - 9: using ionic elements like normal html elements

---
+++ 2- Ionic Component Basics/ionic-angular-course/app.js
@@ -0,0 +1,13 @@
+const reasonInput = document.querySelector("#input-reason");
+const amountInput = document.querySelector("#input-amount");
+const saveBtn = document.querySelector("#btn-save");
+const clearBtn = document.querySelector("#btn-clear");
+
+
+saveBtn.addEventListener('click', () => {
+    console.log('save btn clicked');
+});
+
+clearBtn.addEventListener('click', () => {
+    console.log('clear btn clicked');
+});

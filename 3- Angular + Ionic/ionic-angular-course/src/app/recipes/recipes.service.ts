05ab351070a1a73c2c55ae758e1acd4e81a7f6d3
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2022-08-08 21:59:06 +0200

    Section(angular and ionic) - 20: managing state with services

---
+++ 3- Angular + Ionic/ionic-angular-course/src/app/recipes/recipes.service.ts
@@ -0,0 +1,35 @@
+import {Injectable} from '@angular/core';
+import {IRecipe} from "./recipe.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+  private recipes: IRecipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Schnitzel.JPG/1024px-Schnitzel.JPG',
+      ingredients: ['French Fries', 'Pork Meat', 'Salad']
+    },
+    {
+      id: 'r2',
+      title: 'Spaghetti',
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg/1024px-Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg',
+      ingredients: ['Spaghetti', 'Meat', 'Tomatoes']
+    }
+  ];
+
+  constructor() {
+  }
+
+  public getAllRecipes() {
+    return [...this.recipes];
+  }
+
+  public getRecipe(recipeId: string) {
+    return {...this.recipes.find(recipe => recipe.id === recipeId)};
+  }
+}

677df6e8182b49ff8b7bac04e802bcaac58560c7
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2022-08-10 10:43:14 +0200

    Section(navigation & routing) - 33: edite routing & make routing imports in one place (places routing)

--- 4- Navigation and Routing in Ionic Apps/place-booking-app/src/app/places/offers/offers-routing.module.ts
+++ 4- Navigation and Routing in Ionic Apps/place-booking-app/src/app/places/offers/offers-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
     loadChildren: () => import('./edit-offer/edit-offer.module').then( m => m.EditOfferPageModule)
   },
   {
-    path: 'offer-bookings',
+    path: ':placeId',
     loadChildren: () => import('./offer-bookings/offer-bookings.module').then( m => m.OfferBookingsPageModule)
   }
 ];

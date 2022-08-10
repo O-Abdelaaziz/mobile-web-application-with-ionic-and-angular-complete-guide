ec77fa06fb46a18fa42499d28d91dc3df1b088a5
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2022-08-10 12:25:52 +0200

    Section(navigation & routing) - 37: going back with navcontroller

--- 4- Navigation and Routing in Ionic Apps/place-booking-app/src/app/places/discover/place-detail/place-detail.page.ts
+++ 4- Navigation and Routing in Ionic Apps/place-booking-app/src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {NavController} from "@ionic/angular";
 
 @Component({
   selector: 'app-place-detail',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaceDetailPage implements OnInit {
 
-  constructor() { }
+  constructor(private _router: Router, private _navController: NavController) {
+  }
 
   ngOnInit() {
   }
 
+  onBookPlace() {
+    //this._router.navigate(['/places/tabs/discover']);
+    this._navController.navigateBack('/places/tabs/discover');
+  }
 }

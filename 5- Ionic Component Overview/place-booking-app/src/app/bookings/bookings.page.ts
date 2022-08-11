c2ce8db6bdab3ebcdda420254af56276e36bc40b
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2022-08-11 12:56:00 +0200

    Section(component overview) - 55: swipeable bookings

--- 5- Ionic Component Overview/place-booking-app/src/app/bookings/bookings.page.ts
+++ 5- Ionic Component Overview/place-booking-app/src/app/bookings/bookings.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BookingService} from "./booking.service";
+import {Booking} from "./booking.model";
+import {IonItemSliding} from "@ionic/angular";
 
 @Component({
   selector: 'app-bookings',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bookings.page.scss'],
 })
 export class BookingsPage implements OnInit {
+  public bookings: Booking[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private _bookingService: BookingService) {
   }
 
+  ngOnInit() {
+    this.bookings = this._bookingService._booking;
+  }
+
+  onCancelBooking(id: string, slidingElement: IonItemSliding) {
+    slidingElement.close();
+    console.log("Booking Cancled!");
+  }
 }

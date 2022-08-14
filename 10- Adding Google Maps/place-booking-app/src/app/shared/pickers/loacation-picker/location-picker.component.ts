ba31a0d5fc5f1adfd6d0a085f8e1cb868efa3272
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2022-08-14 22:19:17 +0200

    Section(adding google maps) - 117: finding the addres for a place

--- 10- Adding Google Maps/place-booking-app/src/app/shared/pickers/loacation-picker/location-picker.component.ts
+++ 10- Adding Google Maps/place-booking-app/src/app/shared/pickers/loacation-picker/location-picker.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {ModalController} from '@ionic/angular';
 import {MapModalComponent} from '../../map-modal/map-modal.component';
+import {HttpClient} from '@angular/common/http';
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-location-picker',
@@ -9,7 +11,10 @@ import {MapModalComponent} from '../../map-modal/map-modal.component';
 })
 export class LocationPickerComponent implements OnInit {
 
-  constructor(private _modalController: ModalController) {
+  constructor(
+    private _httpClient: HttpClient,
+    private _modalController: ModalController
+  ) {
   }
 
   ngOnInit() {
@@ -19,10 +24,32 @@ export class LocationPickerComponent implements OnInit {
     this._modalController.create({
       component: MapModalComponent
     }).then((modalElement) => {
-      modalElement.onDidDismiss().then((modelData) => {
-        console.log(modelData.data);
+      modalElement.onDidDismiss().then((modalData) => {
+        if (!modalData.data) {
+          return;
+        }
+        this.getAddress(modalData.data.lat, modalData.data.lng).subscribe(
+          address => {
+            console.log(address);
+          }
+        );
       });
       modalElement.present();
     });
+  }
+
+  private getAddress(lat: number, lng: number) {
+    return this._httpClient
+      .get<any>(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=Your_Api_Key`
+      )
+      .pipe(
+        map(geoData => {
+          if (!geoData || !geoData.results || geoData.results.length === 0) {
+            return null;
+          }
+          return geoData.results[0].formatted_address;
+        })
+      );
   }
 }

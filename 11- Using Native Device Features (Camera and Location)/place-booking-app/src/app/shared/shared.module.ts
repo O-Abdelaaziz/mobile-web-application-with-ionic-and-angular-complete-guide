5454ba2b63c1b1ed61305f1a946381314681eae8
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2022-08-15 21:42:47 +0200

    Section(using native device features) - 129: starting with the image picker

--- 11- Using Native Device Features (Camera and Location)/place-booking-app/src/app/shared/shared.module.ts
+++ 11- Using Native Device Features (Camera and Location)/place-booking-app/src/app/shared/shared.module.ts
@@ -2,13 +2,13 @@ import {NgModule} from '@angular/core';
 import {LocationPickerComponent} from './pickers/loacation-picker/location-picker.component';
 import {MapModalComponent} from './map-modal/map-modal.component';
 import {CommonModule} from '@angular/common';
-import {BrowserModule} from '@angular/platform-browser';
 import {IonicModule} from '@ionic/angular';
+import {ImagePickerComponent} from './pickers/image-picker/image-picker.component';
 
 @NgModule({
-  declarations: [LocationPickerComponent, MapModalComponent],
+  declarations: [LocationPickerComponent, MapModalComponent, ImagePickerComponent],
   imports: [CommonModule, IonicModule.forRoot()],
-  exports: [LocationPickerComponent, MapModalComponent],
+  exports: [LocationPickerComponent, MapModalComponent, ImagePickerComponent],
 })
 export class SharedModule {
 }

00047548c9361e6244d24b8804f7888956355c2e
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2022-08-16 22:34:30 +0200

    Section(adding authentication) - 149: adding autologin

--- 11- Using Native Device Features (Camera and Location)/place-booking-app/src/app/auth/authentication.service.ts
+++ 11- Using Native Device Features (Camera and Location)/place-booking-app/src/app/auth/authentication.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {BehaviorSubject} from 'rxjs';
+import {BehaviorSubject, from} from 'rxjs';
 import {User} from './user.model';
 import {map, tap} from 'rxjs/operators';
 import {Preferences} from '@capacitor/preferences';
@@ -52,6 +52,41 @@ export class AuthenticationService {
       .pipe(
         tap(
           this.setUserDate.bind(this)));
+  }
+
+  autoLogin() {
+    return from(Preferences.get({key: 'authData'})).pipe(
+      map(storedData => {
+        if (!storedData || !storedData.value) {
+          return null;
+        }
+        const parsedData = JSON.parse(storedData.value) as {
+          token: string;
+          tokenExpirationDate: string;
+          userId: string;
+          email: string;
+        };
+        const expirationTime = new Date(parsedData.tokenExpirationDate);
+        if (expirationTime <= new Date()) {
+          return null;
+        }
+        const user = new User(
+          parsedData.userId,
+          parsedData.email,
+          parsedData.token,
+          expirationTime
+        );
+        return user;
+      }),
+      tap(user => {
+        if (user) {
+          this._user.next(user);
+        }
+      }),
+      map(user => {
+        return !!user;
+      })
+    );
   }
 
   public login(email: string, password: string) {
